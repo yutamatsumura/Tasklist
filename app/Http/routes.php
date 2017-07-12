9007fd37bc4c7f875b60104cc4fef1e6b0e3d843
yutamatsumura <yuta.matsumura@bravi.co.jp>
2017-07-12 05:12:46 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
