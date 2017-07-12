ccc7382058ae97791e32905fef6533291ce46229
yutamatsumura <yuta.matsumura@bravi.co.jp>
2017-07-12 07:24:49 +0000

    user login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,7 +22,10 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-
+    
+    protected $redirectTo = '/';        // 追加
+    protected $loginPath = '/login';    // 追加
+    
     /**
      * Create a new authentication controller instance.
      *
