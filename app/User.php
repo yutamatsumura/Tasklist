5f4df3f89ae636f8f2f445afe0f3570f458d6515
yutamatsumura <yuta.matsumura@bravi.co.jp>
2017-07-14 06:26:19 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -90,4 +90,11 @@ class User extends Model implements AuthenticatableContract,
     public function is_following($userId) {
         return $this->followings()->where('follow_id', $userId)->exists();
     }
+    
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()->lists('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
 }
