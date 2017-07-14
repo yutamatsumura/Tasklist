39c8f1fbab31d9918f878fc4c89b5db35270ad5b
yutamatsumura <yuta.matsumura@bravi.co.jp>
2017-07-14 06:10:20 +0000

    follow,unfollow

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use App\Micropost;
-
-class MicropostsController extends Controller
+class UserFollowController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -37,17 +35,10 @@ class MicropostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
-        $this->validate($request, [
-            'content' => 'required|max:255',
-        ]);
-        
-        $request->user()->microposts()->create([
-            'content' => $request->content,
-        ]);
-    
-        return redirect('/');
+        \Auth::user()->follow($id);
+        return redirect()->back();
     }
     /**
      * Display the specified resource.
@@ -91,12 +82,7 @@ class MicropostsController extends Controller
      */
     public function destroy($id)
     {
-        $micropost = Micropost::find($id);
-        
-        if (\Auth::user()->id === $micropost->user_id) {
-            $micropost->delete();
-        }
-        
+        \Auth::user()->unfollow($id);
         return redirect()->back();
     }
 }
